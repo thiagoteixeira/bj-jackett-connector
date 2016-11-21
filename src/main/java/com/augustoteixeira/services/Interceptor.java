9320c957f136b3f075f71e6139299cedfef975ca
Thiago Teixeira <thiagoteixeiraoficial@gmail.com>
2016-11-21 20:42:46 -0300

    Changing timeout to 30s

--- src/main/java/com/augustoteixeira/services/Interceptor.java
+++ src/main/java/com/augustoteixeira/services/Interceptor.java
@@ -34,7 +34,7 @@ public class Interceptor {
 		System.out.println("URI: "+ uri);
 
 		try {
-			Document document = Jsoup.connect(uri).header("Accept", "application/rss+xml, text/rss+xml, text/xml").timeout(20000).get();			
+			Document document = Jsoup.connect(uri).header("Accept", "application/rss+xml, text/rss+xml, text/xml").timeout(30000).get();			
 			String xml = Util.normalizeTexts(document);
 			return xml;
 		} catch (IOException e) {
